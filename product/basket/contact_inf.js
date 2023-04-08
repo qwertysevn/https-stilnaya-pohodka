3113138e89c57a87dc3e0ee98c699ca690dd2fd4
LordlyThief <vostanzunileg1_2@mail.ru>
2023-04-08 04:43:18 +0300

    3

---
+++ product/basket/contact_inf.js
@@ -0,0 +1,17 @@
+const tryButton = document.querySelector('.js_tryButton');
+const tryDisplay = document.querySelector('.js_tryDisplay');
+
+tryButton.onclick = function () {
+    tryDisplay.style.display = "block";
+};
+
+tryDisplay.onclick = function (event) {
+    if (event.target.className.includes('overlay')) {
+        tryDisplay.style.display = 'none';
+    };
+};
+
+
+    $('.js-close-popup').click(function () {
+        $('.all_modal').hide();
+    });
