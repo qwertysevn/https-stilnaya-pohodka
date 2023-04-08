08454b1d099c1ed0336322f7cd6cd6e1f446f926
Egor74687 <2017badamshin@gmail.com>
2023-04-08 09:58:25 +0400

    567

--- m.js
+++ m.js
@@ -11,14 +11,17 @@ entryDisplay.onclick = function (event) {
     };
 };
 
-// const entryButton = document.querySelector('.');
 const divGreen = document.querySelector(".js_div_green");
+const divP = document.querySelector(".js-third");
 
 function changeColor() {
     divGreen.style.color = "#FFF";
     divGreen.style.backgroundColor = "#3D5C4C";
+    divP.style.color = "#FFF";
+    divP.style.backgroundColor = "#3D5C4C";
 }
 
-$('.js-close-modal').click(function () {
+
+$('.js-close-popup').click(function () {
     $('.all_modal').hide();
 });
--- product/size.js
+++ product/size.js
@@ -1,6 +1,9 @@
-const divGreenTo = document.querySelector(".js_div_greenTo");
+const div_Green = document.querySelector(".js_green");
+const div_P = document.querySelector(".js_third");
 
 function changeColor() {
-    divGreenTo.style.color = "#FFF";
-    divGreenTo.style.backgroundColor = "#3D5C4C";
+    div_Green.style.color = "#FFF";
+    div_Green.style.backgroundColor = "#3D5C4C";
+    div_P.style.color = "#FFF";
+    div_P.style.backgroundColor = "#3D5C4C";
 }
